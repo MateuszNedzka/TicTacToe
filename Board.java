0aec8a681dc4bfddabb661c852f42b441f738c97
Mateusz Nędzka <nendzur@gmail.com>
2019-02-01 12:23:39 +0100

    added conditions

--- Board.java
+++ Board.java
@@ -23,4 +23,54 @@ public class Board {
     public void updateBoard(int indexX, int indexY, String kolkoLubKrzyzyk) {
         board3X3[indexX][indexY] = kolkoLubKrzyzyk;
     }
+
+    public void checkRowsIfWin() {
+        if (board3X3[0][0].equals("O") & board3X3[0][1].equals("O") & board3X3[0][2].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[1][0].equals("O") & board3X3[1][1].equals("O") & board3X3[1][2].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[2][0].equals("O") & board3X3[2][1].equals("O") & board3X3[2][2].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[0][0].equals("X") & board3X3[0][1].equals("X") & board3X3[0][2].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else if (board3X3[1][0].equals("X") & board3X3[1][1].equals("X") & board3X3[1][2].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else if (board3X3[2][0].equals("X") & board3X3[2][1].equals("X") & board3X3[2][2].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else {
+            System.out.println("REMIS");
+        }
+    }
+
+    public void checkColsIfWin() {
+        if (board3X3[0][0].equals("O") & board3X3[1][0].equals("O") & board3X3[2][0].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[0][1].equals("O") & board3X3[1][1].equals("O") & board3X3[2][1].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[0][2].equals("O") & board3X3[1][2].equals("O") & board3X3[2][2].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[0][0].equals("X") & board3X3[1][0].equals("X") & board3X3[2][0].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else if (board3X3[0][1].equals("X") & board3X3[1][1].equals("X") & board3X3[2][1].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else if (board3X3[0][2].equals("X") & board3X3[1][2].equals("X") & board3X3[2][2].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else {
+            System.out.println("REMIS");
+        }
+    }
+
+    public void checkDiagonalsIfWin() {
+        if (board3X3[0][0].equals("O") & board3X3[1][1].equals("O") & board3X3[2][2].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[0][2].equals("O") & board3X3[1][1].equals("O") & board3X3[2][0].equals("O")) {
+            System.out.println("Player 'O' Win");
+        } else if (board3X3[0][0].equals("X") & board3X3[1][1].equals("X") & board3X3[2][2].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else if (board3X3[0][2].equals("X") & board3X3[1][1].equals("X") & board3X3[2][0].equals("X")) {
+            System.out.println("Player 'X' Win");
+        } else {
+            System.out.println("REMIS");
+        }
+    }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -5,11 +5,18 @@ public class TicTacToe {
 
         Player player = new Player();
         Board board = new Board();
+
         for (int i = 1; i <= 9; i++) {
             board.showBoard();
             player.playerOMove();
+            board.checkRowsIfWin();
+            board.checkColsIfWin();
+            board.checkDiagonalsIfWin();
             board.showBoard();
             player.playerXMove();
+            board.checkRowsIfWin();
+            board.checkColsIfWin();
+            board.checkDiagonalsIfWin();
             board.showBoard();
         }
     }
